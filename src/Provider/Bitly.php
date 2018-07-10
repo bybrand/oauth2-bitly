795885b6bf8f9ae774d7dd80a7fec02329d86a81
Bernardo Castro <bs.castro@gmail.com>
2018-07-10 17:10:25 -0300

    Create class initial Provider/Bitly.
    - Create test phpunit.

---
+++ src/Provider/Bitly.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Bybrand\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use Psr\Http\Message\ResponseInterface;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+
+class Bitly extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * @var string Key used in a token response to identify the resource owner.
+     */
+    const ACCESS_TOKEN_RESOURCE_OWNER_ID = 'login';
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return 'https://bitly.com/oauth/authorize';
+    }
+
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param array $params
+     *
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return 'https://www.bitly.com/oauth/access_token';
+    }
+
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param AccessToken $token
+     *
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return 'https://api-ssl.bitly.com/v4/groups';
+    }
+
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * @return array
+     */
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
+
+    /**
+     * Check a provider response for errors.
+     *
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (isset($data['error'])) {
+            throw new IdentityProviderException(
+                $data['error'] ?: $response->getReasonPhrase(),
+                $response->getStatusCode(),
+                $response
+            );
+        }
+    }
+
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     *
+     * @return BitlyResourceOwner
+     */
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new BitlyResourceOwner($response);
+    }
+}
---
+++ tests/src/Provider/BitlyTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Bybrand\OAuth2\Client\Test\Provider;
+
+use PHPUnit\Framework\TestCase;
+use Bybrand\OAuth2\Client\Provider\Bitly;
+use Mockery as m;
+
+class BitlyTest extends TestCase
+{
+    protected $provider;
+
+    protected function setUp()
+    {
+        $this->provider = new Bitly([
+            'clientId'     => 'mock_client_id',
+            'clientSecret' => 'mock_secret',
+            'redirectUri'  => 'none',
+        ]);
+    }
+
+    public function tearDown()
+    {
+        m::close();
+        parent::tearDown();
+    }
+
+    /**
+     * @group Bitly
+     * @group Bitly.AuthorizationUrl
+     */
+    public function testAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+        parse_str($uri['query'], $query);
+
+        $this->assertArrayHasKey('client_id', $query);
+        $this->assertArrayHasKey('response_type', $query);
+        $this->assertArrayHasKey('client_id', $query);
+        $this->assertArrayHasKey('redirect_uri', $query);
+        $this->assertArrayHasKey('state', $query);
+        $this->assertArrayHasKey('scope', $query);
+        $this->assertArrayHasKey('approval_prompt', $query);
+        $this->assertNotNull($this->provider->getState());
+    }
+
+    /**
+     * @group Bitly
+     * @group Bitly.GetAuthorizationUrl
+     */
+    public function testGetAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+
+        $this->assertEquals('/oauth/authorize', $uri['path']);
+    }
+
+    /**
+     * @group Bitly
+     * @group Bitly.GetBaseAccessTokenUrl
+     */
+    public function testGetBaseAccessTokenUrl()
+    {
+        $params = [];
+
+        $url = $this->provider->getBaseAccessTokenUrl($params);
+        $uri = parse_url($url);
+
+        $this->assertEquals('/oauth/access_token', $uri['path']);
+    }
+
+    /**
+     * @group Bitly
+     * @group Bitly.GetAccessToken
+     */
+    public function testGetAccessToken()
+    {
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody')
+            ->andReturn('{"access_token": "mock_access_token", "token_type": "bearer", "login": "mock_login", "apiKey": "api_key_id"}');
+        $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(1)->andReturn($response);
+
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider
+            ->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $this->assertEquals('mock_access_token', $token->getToken());
+        $this->assertNull($token->getExpires());
+        $this->assertNull($token->getRefreshToken());
+        $this->assertEquals('mock_login', $token->getResourceOwnerId());
+    }
+}
